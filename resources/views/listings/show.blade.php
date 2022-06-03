867aad18a96c7fd4df6071c739f4a91fe5011da7
batch1 <test.batch1@gmail.com>
2022-06-03 22:54:15 +0700

    jadiin slot layoutnya

--- resources/views/listings/show.blade.php
+++ resources/views/listings/show.blade.php
@@ -1,6 +1,4 @@
-@extends('layout')
-
-@section('content')
+<x-layout>
     @include('partials._search')
 
     <a href="/" class="inline-block text-black ml-4 mb-4"><i class="fa-solid fa-arrow-left"></i> Back
@@ -8,7 +6,7 @@
     <div class="mx-4">
         <x-card class="p-10">
             <div class="flex flex-col items-center justify-center text-center">
-                <img class="w-48 mr-6 mb-6" src="{{asset('images/no-image.png')}}" alt="" />
+                <img class="w-48 mr-6 mb-6" src="{{ asset('images/no-image.png') }}" alt="" />
 
                 <h3 class="text-2xl mb-2">{{ $listing->title }}</h3>
                 <div class="text-xl font-bold mb-4">{{ $listing->company }}</div>
@@ -39,4 +37,4 @@
             </div>
         </x-card>
     </div>
-@endsection
+</x-layout>
